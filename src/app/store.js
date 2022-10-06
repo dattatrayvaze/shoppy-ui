3f1913fa10bd5c3fe0b19a7c1ef3ec021dafdcec
Saylee Phadte <saylee_phadte@persistent.com>
2022-10-06 11:11:34 +0530

    Components added

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../features/user/userSlice';
+import ProductReducer from '../features/Product/productSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    user: userReducer
+    user: userReducer,
+    products: ProductReducer
   },
 });
---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function MultiActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Product Name
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Product Description
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Add to Cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
