1259011ffd0622c554e02480a8899d3c955be9e5
sudikshabhat <112939231+sudikshabhat@users.noreply.github.com>
2022-10-12 15:24:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Counter } from './features/counter/Counter';
 import { User } from './features/user/User';
+import { Categories } from './features/categories/Categories';
 
 function App() {
   return (
     <div className="App">
-      <User></User>
+      <Categories />
+      {/* <User/> */}
     </div>
   );
 }
